2108fb944c7bec926341cd00bf6834160c1c5853
Abdelazim Ali <blackflower225@gmail.com>
2017-11-21 22:17:14 +0200

    add get todos id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+const { ObjectID } = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
@@ -30,6 +30,23 @@ app.get('/todos', (req, res) => {
         res.status(400).send(err);
     })
 });
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send({});
+        }
+        res.status(200).send({ todo });
+
+    }, (err) => {
+        res.status(400).send();
+    }).catch((e) => res.status(400).send())
+});
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
