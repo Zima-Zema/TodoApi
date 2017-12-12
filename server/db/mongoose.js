1229dd339797807673ad64dc3007a4c6afe844c3
Abdelazim Ali <blackflower225@gmail.com>
2017-12-12 21:01:36 +0200

    fix deploy uri

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://blackflower225:Alph@159@ds046027.mlab.com:46027/todoapp', {
+mongoose.connect('mongodb://abdelazim:Admin.369@ds046027.mlab.com:46027/todoapp', {
     useMongoClient: true
 });
 
