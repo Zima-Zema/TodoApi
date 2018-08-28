38e6643742479ba34b59b873dc9c3c49291788d7
Abdelazim Ali <abdelazim.ali.ali@gmail.com>
2018-08-28 17:53:57 +0200

    finish login route

--- server/models/user.js
+++ server/models/user.js
@@ -71,7 +71,19 @@ UserSchema.statics.findByToken = function (token) {
         'tokens.access': 'auth'
     })
 }
-
+UserSchema.statics.findByCredentials = function (email, password) {
+    return this.findOne({email}).then((user)=>{
+        if (!user) {
+            return Promise.reject();
+        }
+        return bcrypt.compare(password,user.password).then((res)=>{
+            if (!res) {
+                return Promise.reject();
+            }
+            return user;
+        })
+    })
+}
 UserSchema.pre("save", function (next) {
     if (this.isModified('password')) {
         bcrypt.genSalt(10).then(salt => {
